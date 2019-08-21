fc1343e3c8dc2a5623c9b421933a44c891296ae5
Sir-Williams <sirjohnwilliams@live.com>
2019-08-20 19:00:43 -0700

    added router folder and post router; setup server

---
+++ routers/postRouter.js
@@ -0,0 +1,2 @@
+const express = require('express')
+const postDb = require('../data/dbConfig.js')
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const postRouter = require('./routers/postRouter.js')
+
+// const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('api/posts', postRouter);
 
 module.exports = server;
