34afc5d754184f613ace9a65cfe20c2a152b9219
Sir-Williams <sirjohnwilliams@live.com>
2019-08-21 16:18:55 -0700

    fixed issue with accounts not being populated; get and git/id work

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const accountDb = require('../data/dbConfig.js')
-const router = express.Router()
+const express = require('express');
+const accountDb = require('../data/dbConfig.js');
+const router = express.Router();
 
 router.get('/', (req, res) => {
     accountDb('accounts')
-        .then(account => {
-            res.status(200).json(account)
+        .then(acc => {
+            res.status(200).json(acc)
         })
         .catch(err => {
-            res.status(500).json({ error: 'Error getting accounts'})
+            res.status(500).json({ error: 'Error getting accounts' })
         })
 })
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    accountDb('accounts')
+        .where({ id })
+        .then(acc => {
+            res.status(200).json(acc)
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Error finding account' })
+        })
+  })
+
+
 
 module.exports = router;
